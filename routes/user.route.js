162249a1001e935a64bbf13341416c9aaebccc0b
waqar-313 <157794826+waqar-313@users.noreply.github.com>
2025-06-15 21:04:03 +0500

    Uploading the Job portal website

---
+++ routes/user.route.js
@@ -0,0 +1,19 @@
+import express from "express";
+import { login, logout, register, updateProfile } from "../controllers/user.controller.js";
+import isAuthenticated from "../middlewares/isAuthenticated.js";
+import { singleUpload } from "../middlewares/mutler.js";
+ 
+const router = express.Router();
+
+router.route("/register").post((req, res, next) => {
+    console.log("Request received at /register");
+    next();
+}, singleUpload, register);
+
+// router.route("/register").post(singleUpload,register);
+router.route("/login").post(login);
+router.route("/logout").get(logout);
+router.route("/profile/update").post(isAuthenticated,singleUpload,updateProfile);
+
+export default router;
+
